9dbc38d192eca6f3a7717458bf4abbb6f4ec0521
S29 <nots29vv@gmail.com>
2024-02-08 11:10:29 +0100

    Add .env to .gitignore

--- main/main.go
+++ main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +10,11 @@ import (
 )
 
 func main() {
+	startBot()
+	startServer()
+}
+
+func startBot() {
 	req_dur := os.Getenv("REQUEST_DURATION")
 	// how can you create an alternate dev environment
 	// if len(req_dur) == 0 {
@@ -22,8 +26,7 @@ func main() {
 	}
 	dur := time.Duration(val) * time.Second
 	t := time.NewTicker(dur)
-	fmt.Println(t)
-	fmt.Println(dur)
+
 	go func(t <-chan time.Time) {
 		for {
 			select {
@@ -32,29 +35,11 @@ func main() {
 			}
 		}
 	}(t.C)
-	http.ListenAndServe(":8090", nil)
-	// StartHTTP()
-	// gin.SetMode(gin.ReleaseMode)
-	// router := gin.New()
-	// port := os.Getenv("PORT")
-	// if port == "" {
-	// 	port = "8080"
-	// }
-	// if err := router.Run(":" + port); err != nil {
-	// 	log.Panicf("error: %s", err)
-	// }
 }
 
-// func StartHTTP() {
-// 	s := &http.Server{
-// 		Addr: ":8080",
-// 		// Handler:        myHandler,
-// 		ReadTimeout:    10 * time.Second,
-// 		WriteTimeout:   10 * time.Second,
-// 		MaxHeaderBytes: 1 << 20,
-// 	}
-// 	log.Fatal(s.ListenAndServe())
-// }
+func startServer() {
+	http.ListenAndServe(":8090", nil)
+}
 
 // i would need to get the logs of this service
 /**
