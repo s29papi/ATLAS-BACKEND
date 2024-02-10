ef191b4bf0093ff2661232cda7b957bdcc0c9517
S29 <nots29vv@gmail.com>
2024-02-10 09:02:46 +0100

    clean up

--- main/main.go
+++ main/main.go
@@ -26,20 +26,3 @@ func startBot() {
 func startServer() {
 	http.ListenAndServe(":8090", nil)
 }
-
-// i would need to get the logs of this service
-/**
-* we have a single go routine
-* bot.Start() --> 3 go routines ---> a channel and an os.Exit()
-* 1st go routine is called tick. has a for loop and select statement which blocks
-* pending when its channels recieve a value
-*
-*
-*
- */
-//  curl http://hub.freefarcasterhub.com:2281/v1/castsByMention?fid=318902
-
-// we have a free hub
-// a frontend vercel
-//
-// curl http://arena.wield.co/v1/castsByMention?fid=318902
