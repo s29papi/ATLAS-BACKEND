2648ed880fc8764cc752679bd6cfaa6ffca63fff
S29 <nots29vv@gmail.com>
2024-02-13 14:14:42 +0100

    set og params

--- frontend/app/api/frame/route.ts
+++ frontend/app/api/frame/route.ts
@@ -14,7 +14,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
           <meta property="fc:frame:image" content="https://wag3r-bot.vercel.app/stadium-second-page.png"/>
           <meta property="fc:frame:button:1" content="Wager" />
           <meta property="fc:frame:button:1:action" content="post"/>
-          <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/frame"/>
+          <meta property="fc:frame:post_url" content="https://wag3r-bot.vercel.app/api/frame/wager"/>
       </head></html>`);
   }
   // wager: is the second buttonId
