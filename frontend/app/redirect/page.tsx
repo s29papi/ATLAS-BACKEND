94e698f3be840d30b0b5d9d8974109bc51bd77c2
S29 <nots29vv@gmail.com>
2024-02-13 16:03:50 +0100

    set og params

--- frontend/app/redirect/page.tsx
+++ frontend/app/redirect/page.tsx
@@ -6,17 +6,43 @@ export default function Redirect() {
     const router = useRouter();
 
     useEffect(() => {
-        const redirectUrl = '';
+        const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+          // Cancel the default behavior of closing the tab
+          event.preventDefault();
+          // Chrome requires the returnValue to be set
+          event.returnValue = '';
+        };
+    
+        // Add event listener to beforeunload event
+        window.addEventListener('beforeunload', handleBeforeUnload);
+    
+        return () => {
+          // Remove the event listener when component unmounts
+          window.removeEventListener('beforeunload', handleBeforeUnload);
+        };
+      }, []);
 
-        // Perform the redirect
-        window.location.href = redirectUrl; // For a full page reload redirect
-        // Or use Next.js router for client-side redirect (comment out the line above if using this)
-        // router.push(youtubeUrl);
-    }, [router]);
+      const handleCloseButtonClick = () => {
+        // Close the current tab
+        window.close();
+      };
+
 
     return (
         <div>
             <p>Redirecting...</p>
+            <button onClick={handleCloseButtonClick}>Close Tab</button>
         </div>
     );
 }
+
+
+
+    // useEffect(() => {
+    //     const redirectUrl = '';
+
+    //     // Perform the redirect
+    //     window.location.href = redirectUrl; // For a full page reload redirect
+    //     // Or use Next.js router for client-side redirect (comment out the line above if using this)
+    //     // router.push(youtubeUrl);
+    // }, [router]);
