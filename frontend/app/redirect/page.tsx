f15d650cc425ba412527adf97da70564160f8610
S29 <nots29vv@gmail.com>
2024-02-13 15:36:19 +0100

    set og params

--- frontend/app/redirect/page.tsx
+++ frontend/app/redirect/page.tsx
@@ -2,22 +2,14 @@
 import {useRouter} from "next/navigation";
 import {useEffect} from "react";
 
-export default function RedirectPage() {
+export default function Redirect() {
     const router = useRouter();
 
     useEffect(() => {
         const redirectUrl = 'https://twitter.com';
 
-        const width = 600;
-        const height = 400;
-        const left = (window.innerWidth - width) / 2;
-        const top = (window.innerHeight - height) / 2;
-        const options = `width=${width},height=${height},left=${left},top=${top}`;
-    
-
         // Perform the redirect
-        // window.location.href = redirectUrl; // For a full page reload redirect
-        window.open(redirectUrl)
+        window.location.href = redirectUrl; // For a full page reload redirect
         // Or use Next.js router for client-side redirect (comment out the line above if using this)
         // router.push(youtubeUrl);
     }, [router]);
