c91e67247917382ae092b9563554f30d798f309d
S29 <nots29vv@gmail.com>
2024-02-13 14:39:39 +0100

    set og params

--- frontend/app/redirect/page.tsx
+++ frontend/app/redirect/page.tsx
@@ -2,7 +2,7 @@
 import {useRouter} from "next/navigation";
 import {useEffect} from "react";
 
-export default function Redirect() {
+export default function RedirectPage() {
     const router = useRouter();
 
     useEffect(() => {
