b2e6a5547794f288600e439ec6d90e2c2c803564
S29 <nots29vv@gmail.com>
2024-02-13 13:31:10 +0100

    set og params

--- frontend/app/api/frame/route.ts
+++ frontend/app/api/frame/route.ts
@@ -11,7 +11,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
           return new NextResponse(`<!DOCTYPE html><html><head>
           <title>Wager</title>
           <meta property="fc:frame" content="vNext" />
-          <meta property="fc:frame:image" content="https://wag3r-bot.vercel.app/second-page.png"/>
+          <meta property="fc:frame:image" content="https://wag3r-bot.vercel.app/stadium-second-page.png"/>
           <meta property="fc:frame:button:1" content="Wager" />
           <meta property="fc:frame:button:1:action" content="post"/>
           <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/frame"/>
