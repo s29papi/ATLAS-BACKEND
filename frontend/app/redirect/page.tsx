27b343e2c546a3e2ad03911e3b7d6edb4e1e5cd4
S29 <nots29vv@gmail.com>
2024-02-14 17:22:05 +0100

    set og params

--- frontend/app/redirect/page.tsx
+++ frontend/app/redirect/page.tsx
@@ -1,64 +1,64 @@
 'use client';
 
-export default function RedirectPage() {
+// export default function RedirectPage() {
 
-}
-// import {useRouter} from "next/navigation";
-// import {useEffect} from "react";
-// // import Connect from '../../components/Connect'
-
-// // declare global {
-// //     interface Window {
-// //       ethereum?: any;
-// //     }
-// //   } 
-
-// export default function Redirect() {
-//     const router = useRouter();
-
-//     useEffect(() => {
-//         const handleBeforeUnload = (event: BeforeUnloadEvent) => {
-//           // Cancel the default behavior of closing the tab
-//           event.preventDefault();
-//           // Chrome requires the returnValue to be set
-//           event.returnValue = '';
-//         };
-    
-//         // Add event listener to beforeunload event
-//         window.addEventListener('beforeunload', handleBeforeUnload);
-    
-//         return () => {
-//           // Remove the event listener when component unmounts
-//           window.removeEventListener('beforeunload', handleBeforeUnload);
-//         };
-//       }, []);
-
-//     useEffect(() => {
-//         async function openWallet() {
-          
-//             // const [account] = await window.ethereum.request({
-//             //     method: 'eth_requestAccounts',
-//             //   });
-//             // return account;
-//         }
-
-//         openWallet()
-//     })
-
-//       const handleCloseButtonClick = () => {
-//         // Close the current tab
-//         window.close();
-//       };
-
-
-//     return (
-//         <div>
-//             <p>Redirecting...</p>
-//             {/* <Connect/> */}
-//             <button onClick={handleCloseButtonClick}>Close Tab</button>
-//         </div>
-//     );
 // }
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
+import Connect from '../../components/Connect'
+
+// declare global {
+//     interface Window {
+//       ethereum?: any;
+//     }
+//   } 
+
+export default function Redirect() {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+          // Cancel the default behavior of closing the tab
+          event.preventDefault();
+          // Chrome requires the returnValue to be set
+          event.returnValue = '';
+        };
+    
+        // Add event listener to beforeunload event
+        window.addEventListener('beforeunload', handleBeforeUnload);
+    
+        return () => {
+          // Remove the event listener when component unmounts
+          window.removeEventListener('beforeunload', handleBeforeUnload);
+        };
+      }, []);
+
+    useEffect(() => {
+        async function openWallet() {
+          
+            // const [account] = await window.ethereum.request({
+            //     method: 'eth_requestAccounts',
+            //   });
+            // return account;
+        }
+
+        openWallet()
+    })
+
+      const handleCloseButtonClick = () => {
+        // Close the current tab
+        window.close();
+      };
+
+
+    return (
+        <div>
+            <p>Redirecting...</p>
+            <Connect/>
+            <button onClick={handleCloseButtonClick}>Close Tab</button>
+        </div>
+    );
+}
 
 
 
