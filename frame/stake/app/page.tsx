a81fdbce13aed32b113cb63e2a9b4798b760baab
S29 <nots29vv@gmail.com>
2024-02-15 21:24:53 +0100

    stake frame image

--- frame/stake/app/page.tsx
+++ frame/stake/app/page.tsx
@@ -1,39 +1,58 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
-import type { Metadata } from 'next';
+import type { Metadata, ResolvingMetadata } from 'next'
+
+type Props = {
+  params: { gameId: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+ 
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> { 
+  const gameId = params.gameId;
+  let postUrl = "https://wag3r-bot.vercel.app/api?gameId=" + `${gameId}`;
+
+  const frameMetadata = getFrameMetadata({
+    buttons: [
+        {label: 'Accept Challenge & Stake Tokens', action: 'post'},
+    ],
+    image: 'https://wag3r-bot-stake.vercel.app/stadium-figma-test-1.png',
+    post_url: postUrl,
+  });
+
+  return {
+    title: 'Stake By Versus.',
+    description: 'Frontend Stake Management for Versus App.',
+    openGraph: {
+      title: 'Stake By Versus.',
+      description: 'Frontend Stake Management for Versus App.',
+      images: [`https://wag3r-bot-stake.vercel.app/stadium-figma-test-1.png`],
+    },
+    other: {
+      ...frameMetadata,
+    }
+  }
+}
 
 
 
-const frameMetadata = getFrameMetadata({
-  buttons: [
-      {label: 'Accept Challenge & Stake Tokens', action: 'post'},
-  ],
-  image: 'https://wag3r-bot-stake.vercel.app/stadium-figma-test-1.png',
-  post_url: 'https://wag3r-bot.vercel.app/api/frame',
-});
-
-export const metadata: Metadata = {
-  title: 'Refuel-Frame by socket.',
-  description: 'Follow this user, Like the post, and Refuel.',
-  openGraph: {
-    title: 'Refuel-Frame by socket.',
-    description: 'Follow this user, Like the post, and Refuel.',
-    images: [`https://wag3r-bot-stake.vercel.app/stadium-figma-test-1a.png`],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
+// export const metadata: Metadata = {
+//   title: 'Refuel-Frame by socket.',
+//   description: 'Follow this user, Like the post, and Refuel.',
+//   openGraph: {
+//     title: 'Refuel-Frame by socket.',
+//     description: 'Follow this user, Like the post, and Refuel.',
+//     images: [`https://wag3r-bot-stake.vercel.app/stadium-figma-test-1.png`],
+//   },
+//   other: {
+//     ...frameMetadata,
+//   },
+// };
 
 
 
-// export default async function Page(props: Props) {
-
-//   return (
-//     <>
-//       <h1>Refuel-Frame by socket.</h1>
-//     </>
-//   );
-// }
 
 
 export default function Page({
