23b6d141f56453b3e7dc97d07844013751f1dc31
S29 <nots29vv@gmail.com>
2024-02-15 06:14:03 +0100

    log transaction hash

--- frontend/app/api/frame/route.ts
+++ frontend/app/api/frame/route.ts
@@ -3,20 +3,20 @@ import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
-  let getParams = req.nextUrl.searchParams
+
   
   const searchParams = req.nextUrl.searchParams
   const gameId:any = searchParams.get("game-id")
 
   const buttonId = body.untrustedData.buttonIndex;
-
+  let bixi = "56"
   // wager: is the first buttonId
   if (buttonId == 1) {
           return new NextResponse(`<!DOCTYPE html><html><head>
           <title>Wager</title>
           <meta property="fc:frame" content="vNext" />
           <meta property="fc:frame:image" content="https://wag3r-bot.vercel.app/stadium-second-page.png"/>
-          <meta property="fc:frame:button:1" content="Wager" />
+          <meta property="fc:frame:button:1" content="${bixi}" />
           <meta property="fc:frame:button:1:action" content="post"/>
           <meta property="fc:frame:post_url" content="https://wag3r-bot.vercel.app/api/frame/wager?gameId=${gameId}"/>
       </head></html>`);
