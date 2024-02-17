71fe0eaca65e48e038ca1067c1acb33b43eca157
S29 <nots29vv@gmail.com>
2024-02-17 23:56:55 +0100

    match og

--- frame/match/app/api/og/route.tsx
+++ frame/match/app/api/og/route.tsx
@@ -19,7 +19,7 @@ export async function GET() {
             backgroundImage: `url(${"https://wag3r-bot.vercel.app/stake.svg"}),`
           }}
         >
-          Hello!
+          
         </div>
       )
     )
