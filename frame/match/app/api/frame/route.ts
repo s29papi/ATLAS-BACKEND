09d00801ba483bb83b1fded138b08d5ff3ea2e30
S29 <nots29vv@gmail.com>
2024-02-17 18:53:39 +0100

    match og

--- frame/match/app/api/frame/route.ts
+++ frame/match/app/api/frame/route.ts
@@ -22,14 +22,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     // if already staked and value is >= game amount
 
     // if already taked and value is less than 
-    return new NextResponse(`<!DOCTYPE html><html><head>
-                <title>Start My Match</title>
-                <meta property="fc:frame" content="vNext" />
-                <meta property="fc:frame:image" content="https://wag3r-bot.vercel.app/api/og"/>
-                <meta property="fc:frame:button:1" content="Start My Match" />
-                <meta property="fc:frame:button:1:action" content="post"/>
-                <meta property="fc:frame:post_url" content=""/>
-            </head></html>`);
+    return ifAccountBalanceIsEqualGreaterStakeAmount()
     // return NextResponse.redirect("https://wag3r-bot.vercel.app/~/viewtx", {status: 302});
   }
   // wager: is the second buttonId
@@ -57,7 +50,16 @@ export const dynamic = 'force-dynamic';
 
 
 
-
+function ifAccountBalanceIsEqualGreaterStakeAmount() {
+    return new NextResponse(`<!DOCTYPE html><html><head>
+            <title>Start My Match</title>
+            <meta property="fc:frame" content="vNext" />
+            <meta property="fc:frame:image" content="https://wag3r-bot.vercel.app/api/og"/>
+            <meta property="fc:frame:button:1" content="Start My Match" />
+            <meta property="fc:frame:button:1:action" content="post"/>
+            <meta property="fc:frame:post_url" content=""/>
+        </head></html>`);
+}
 
 
 
