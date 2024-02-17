d740dbfc6b0580cf40b83a4583fb853384f88913
S29 <nots29vv@gmail.com>
2024-02-17 20:23:55 +0100

    match og

--- frame/match/app/api/frame/stake/route.ts
+++ frame/match/app/api/frame/stake/route.ts
@@ -20,14 +20,14 @@ export const dynamic = 'force-dynamic';
 
 
 function handlesStake2StartMatch() {
-    return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>Start My Match</title>
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="https://wag3r-bot.vercel.app/A-New-Challenger-Has-Entered-The-Ring-Resize.png"/>
-    <meta property="fc:frame:button:1" content="Start My Match" />
-    <meta property="fc:frame:button:1:action" content="post"/>
-    <meta property="fc:frame:post_url" content="https://wag3r-bot.vercel.app/"/>
-</head></html>`);
+                return new NextResponse(`<!DOCTYPE html><html><head>
+                <title>Start My Match</title>
+                <meta property="fc:frame" content="vNext" />
+                <meta property="fc:frame:image" content="https://wag3r-bot.vercel.app/A-New-Challenger-Has-Entered-The-Ring-Resize.png"/>
+                <meta property="fc:frame:button:1" content="Start My Match" />
+                <meta property="fc:frame:button:1:action" content="post"/>
+                <meta property="fc:frame:post_url" content="https://wag3r-bot.vercel.app/api/frame/start-match"/>
+            </head></html>`);
 }
 
 
