8849215b6bd149291f7819b76172c295f4e25815
S29 <nots29vv@gmail.com>
2024-02-17 19:31:46 +0100

    match og

--- frame/match/app/api/frame/stake/route.ts
+++ frame/match/app/api/frame/stake/route.ts
@@ -3,28 +3,12 @@ import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
-
-  
-  const searchParams = req.nextUrl.searchParams
-  const gameId:any = searchParams.get("game-id")
-
   const buttonId = body.untrustedData.buttonIndex;
-  
-  
-  if (buttonId == 1) {
-    // this is in one of three states
 
-    // get the value from the bot of the user
-    // get the value of the game stake amount
+// wager: is the second buttonId
+//   const searchParams = req.nextUrl.searchParams
+//   const gameId:any = searchParams.get("game-id")
 
-    // if it hasn't staked (this values are fetched from golang)
-
-    // if already staked and value is >= game amount
-
-    // if already taked and value is less than 
-    return NextResponse.redirect("https://wag3r-bot.vercel.app/")
-  }
-  // wager: is the second buttonId
   if (buttonId == 2) {
     return new NextResponse(`<!DOCTYPE html><html><head>
           <title>Start My Match</title>
@@ -36,9 +20,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       </head></html>`);
   }
 
-return new NextResponse(`<!DOCTYPE html><html><head>
-  <title>Button doesnt exist</title>
-</head></html>`);
+  return NextResponse.redirect("https://wag3r-bot.vercel.app/")
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
