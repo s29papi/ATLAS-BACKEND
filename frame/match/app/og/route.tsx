848da256b7b4dde5bfeff316488b4da4d8434ddf
S29 <nots29vv@gmail.com>
2024-02-18 07:29:26 +0100

    match og

--- frame/match/app/og/route.tsx
+++ frame/match/app/og/route.tsx
@@ -1,11 +1,15 @@
 import { ImageResponse } from 'next/server'
 // App router includes @vercel/og.
 // No need to install it.
-import base from '../../public/base.png'
+// import base from '../../public/base.png'
 export const runtime = 'edge';
 
 export async function GET() {
-  console.log(base)
+  const imageData = await fetch(new URL('../../public/base.png', import.meta.url)).then(
+    (res) => res.arrayBuffer(),
+  );
+ 
+  console.log(imageData)
     return new ImageResponse(
       (
         <div
