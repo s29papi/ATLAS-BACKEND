672bb9e24df978f3c0848cf8f044d397ec17b294
S29 <nots29vv@gmail.com>
2024-02-18 12:02:51 +0100

    match og

--- frame/match/app/og/route.tsx
+++ frame/match/app/og/route.tsx
@@ -3,47 +3,17 @@
 // @ts-nocheck
 import { ImageResponse } from 'next/server'
 
-import Image  from 'next/image'
-
-import base from '../../public/base.png'
-// App router includes @vercel/og.
-// No need to install it.
-// import base from '../../public/base.png'
 export const runtime = 'edge';
 
 
-export async function GET() {
+export async function GET(req: Request) {
   const imageData = await fetch(new URL('../../public/base.png', import.meta.url)).then(
     (res) => res.arrayBuffer(),
   );
+  return new ImageResponse (
 
 
-  console.log(imageData)
-    return new ImageResponse(
-      (
-        <div
-          // style={{
-          //   width: "100vw",
-          //   height: "100vh",
-          //   display: 'flex',
-          //   alignItems: 'center',
-          //   justifyContent: 'center',
-          //   fontSize: 128,
-          //   // background: 'lavender',
-          //   // backgroundImage: `url(${}),`
-          //   backgroundPosition: "center",
-          //   backgroundSize: "cover",
-          //   backgroundRepeat: "no-repeat",
-          // }}
-        >
-          <Image alt='Base Stake Page' src={base} style={{maxWidth: '100%', height: 'auto'}}/>
-          {/* Bizzy */}
-          {/* <img width="800" height="419" src={imageData}  /> */}
-        </div>
-      ),
-      {
-        width: 800,
-        height: 419,
-      }
+      <div>bij</div>
+   
     )
   }
