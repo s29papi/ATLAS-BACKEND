22735e13b3ee7c35c5a806551cdd836a74bb5662
S29 <nots29vv@gmail.com>
2024-02-18 12:25:12 +0100

    ...

--- frame/match/app/og/route.tsx
+++ frame/match/app/og/route.tsx
@@ -13,7 +13,7 @@ export async function GET(req: Request) {
   return new ImageResponse (
     <div>
        <img src={imageData} width={300} height={217} />
-       <div> Stake 250</div>
+       <div> Stake 20 usdc</div>
     </div> 
     )
   }
