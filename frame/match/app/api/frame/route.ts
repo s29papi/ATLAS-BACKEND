1f054d3a9368dcb79bf8fe556a71b434e3edbf67
S29 <nots29vv@gmail.com>
2024-02-18 07:14:04 +0100

    match og

--- frame/match/app/api/frame/route.ts
+++ frame/match/app/api/frame/route.ts
@@ -55,7 +55,7 @@ function ifAccountBalanceIsEqualGreaterStakeAmount() {
     return new NextResponse(`<!DOCTYPE html><html><head>
             <title>Start My Match</title>
             <meta property="fc:frame" content="vNext" />
-            <meta property="fc:frame:image" content="https://wag3r-bot.vercel.app/api/og"/>
+            <meta property="fc:frame:image" content="https://wag3r-bot.vercel.app/og"/>
             <meta property="fc:frame:button:1" content="Back" />
             <meta property="fc:frame:button:1:action" content="post"/>
             <meta property="fc:frame:button:2" content="Stake" />
---
+++ frame/match/app/og/route.tsx
@@ -0,0 +1,26 @@
+import { ImageResponse } from 'next/server'
+// App router includes @vercel/og.
+// No need to install it.
+
+export const runtime = 'edge';
+
+export async function GET() {
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: 128,
+            background: 'lavender',
+            // backgroundImage: `url(${}),`
+          }}
+        >
+          EARTH DAY! EARTH DAY! EARTH DAY! EARTH DAY!
+        </div>
+      )
+    )
+  }
