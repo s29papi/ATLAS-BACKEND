7aa765a172dbe36b22d87b8cc587dd750a87d61c
S29 <nots29vv@gmail.com>
2024-02-19 23:37:16 +0100

    commented env variables

--- main/main.go
+++ main/main.go
@@ -2,11 +2,6 @@ package main
 
 import (
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/s29papi/wag3r-bot/worker"
 )
 
 func main() {
@@ -16,12 +11,12 @@ func main() {
 
 var id = 318902
 
-func startBot() {
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
-	bot := worker.NewWorker(signalChan)
-	go bot.Start()
-}
+// func startBot() {
+// 	signalChan := make(chan os.Signal, 1)
+// 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+// 	bot := worker.NewWorker(signalChan)
+// 	go bot.Start()
+// }
 
 func startServer() {
 	http.ListenAndServe(":8090", nil)
