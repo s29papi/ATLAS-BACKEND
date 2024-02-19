f56e27d0edfe59026652743036d9d406c70ee34d
S29 <nots29vv@gmail.com>
2024-02-19 04:04:23 +0100

    account frame

--- frame/match/app/og/account/route.tsx
+++ frame/match/app/og/account/route.tsx
@@ -10,16 +10,29 @@ export async function GET(req: Request) {
   const imageData = await fetch(new URL('../../../public/base.png', import.meta.url)).then(
     (res) => res.arrayBuffer(),
   );
-  return new ImageResponse (
+  const geistData = await fetch(new URL('../../../public/Geist-Bold-BF6569491da5a14.otf', import.meta.url)).then(
+    (res) => res.arrayBuffer(),
+  );
+  return new ImageResponse ((
             <div style={{position: 'relative', display: 'flex'}}>
-                <img 
-                    src={imageData}
-                 />
-                <div style={{display: 'flex', position: 'absolute', top: '65%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: '#66757F', fontSize: '72px', fontWeight: '1200'}}> 
-                        <span style={{fontStyle: 'italic'}}>BALANCE $ 20 USDC</span>
-                </div>
-              </div>
+                    <img 
+                        src={imageData}
+                    />
+                    <div style={{display: 'flex', position: 'absolute', top: '65%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: '#66757F', fontSize: '72px', fontWeight: '1200'}}> 
+                            <span style={{fontStyle: 'italic'}}>BALANCE $ 20 USDC</span>
+                    </div>
+             </div>
+        ),
+        {
+            fonts: [
+                {
+                    name: "Geist",
+                    data: geistData,
+                    style: 'italic'
+                },
+            ],
+        }
     )
   }
 
-      
+          
