20750bd3f8e6952923d9b60a1028d354e11931a9
S29 <nots29vv@gmail.com>
2024-02-26 14:12:22 +0100

    trying to re-up landing frame

--- frame/match/app/og/begining/route.tsx
+++ frame/match/app/og/begining/route.tsx
@@ -10,9 +10,9 @@ export async function GET(req: Request) {
     const imageData = await fetch(new URL('../../../public/You-Vs-Me-Rescale.png', import.meta.url)).then(
         (res) => res.arrayBuffer(),
       );
-    const pfpData = await fetch(new URL('https://i.imgur.com/bwzJfrR.jpg', import.meta.url)).then(
-        (res) => res.arrayBuffer(),
-      );
+    // const pfpData = await fetch(new URL('https://i.imgur.com/bwzJfrR.jpg', import.meta.url)).then(
+    //     (res) => res.arrayBuffer(),
+    //   );
     const interData = await fetch(new URL('../../../public/Inter-Regular.ttf', import.meta.url)).then(
         (res) => res.arrayBuffer(),
       );
@@ -31,9 +31,10 @@ export async function GET(req: Request) {
                                 <div tw="flex flex-col text-xl sm:text-3xl mt-6 mr-6 ml-6 mb-6 font-bold tracking-tight text-black text-left">
                                     <span tw="flex flex-col text-lg sm:text-xl md:flex-row w-full py-12 px-4 justify-between p-8" style={{fontFamily: 'Inter-Bold'}}>
                                         <span tw="bottom-16 right-11" style={{fontFamily: 'Inter-Bold', borderRadius: "50%", overflow: 'hidden'}}>
-                                            <img tw="w-[180px] h-[182px]"
+                                            {/* <img tw="w-[180px] h-[182px]"
                                                     src={pfpData}
-                                            /> 
+                                            />  */}
+                                            PFP
                                         </span>  
                                         <span tw="flex flex-col bottom-7" style={{fontFamily: 'Inter-Regular'}}>
                                            <span tw="text-5xl text-gray-400">{stakeAmount} open challenge</span>
