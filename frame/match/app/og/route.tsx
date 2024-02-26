0a6911d097149c204738d27f68d73c384bf58a22
S29 <nots29vv@gmail.com>
2024-02-26 12:31:37 +0100

    changes to /begin

--- frame/match/app/og/route.tsx
+++ frame/match/app/og/route.tsx
@@ -5,40 +5,37 @@ import { ImageResponse } from 'next/server'
 
 export const runtime = 'edge';
 
-const imageData = await fetch(new URL('../../public/base.png', import.meta.url)).then(
-  (res) => res.arrayBuffer(),
-);
 
-export async function GET(req: Request) : Promise<Response> {
-
-  return stakingTing()
+export async function GET(req: Request) {
+  const imageData = await fetch(new URL('../../public/base.png', import.meta.url)).then(
+    (res) => res.arrayBuffer(),
+  );
+  return new ImageResponse (
+        (
+          {staking()}
+        )
+    )
   }
 
-function stakingTing() : Promise<ImageResponse> {
-  return new ImageResponse (
-    <div style={{position: 'relative', display: 'flex'}}>
-        <img 
-            src={imageData}
-         />
-        <div style={{display: 'flex', position: 'absolute', top: '65%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: '#66757F', fontSize: '72px', fontWeight: '1200'}}> 
-                <span style={{fontStyle: 'italic'}}>STAKE $ 20 USDC</span>
+
+  function staking() {
+      return (
+          <div style={{position: 'relative', display: 'flex'}}>
+          <img 
+              src={imageData}
+          />
+          <div style={{display: 'flex', position: 'absolute', top: '65%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: '#66757F', fontSize: '72px', fontWeight: '1200'}}> 
+                  <span style={{fontStyle: 'italic'}}>STAKE $ 20 USDC</span>
+          </div>
         </div>
-      </div>
-)
-}
-
-
-
-// export async function GET(req: Request) {
-
-//   return new ImageResponse (
-//             <div style={{position: 'relative', display: 'flex'}}>
-//                 <img 
-//                     src={imageData}
-//                  />
-//                 <div style={{display: 'flex', position: 'absolute', top: '65%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: '#66757F', fontSize: '72px', fontWeight: '1200'}}> 
-//                         <span style={{fontStyle: 'italic'}}>STAKE $ 20 USDC</span>
-//                 </div>
-//               </div>
-//     )
-//   }
+      )
+  }
+      
+//   <div style={{position: 'relative', display: 'flex'}}>
+//   <img 
+//       src={imageData}
+//    />
+//   <div style={{display: 'flex', position: 'absolute', top: '65%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: '#66757F', fontSize: '72px', fontWeight: '1200'}}> 
+//           <span style={{fontStyle: 'italic'}}>STAKE $ 20 USDC</span>
+//   </div>
+// </div>
