b8f7c3e176164e31a33d4cbfbd9283e3027b1bde
S29 <nots29vv@gmail.com>
2024-02-26 14:06:25 +0100

    trying to re-up landing frame

--- frame/match/app/og/landingframe/route.tsx
+++ frame/match/app/og/landingframe/route.tsx
@@ -10,12 +10,30 @@ export async function GET(req: Request) {
     const imageData = await fetch(new URL('../../../public/You-Vs-Me-Rescale.png', import.meta.url)).then(
         (res) => res.arrayBuffer(),
       );
+    const pfpData = await fetch(new URL('https://i.imgur.com/bwzJfrR.jpg', import.meta.url)).then(
+        (res) => res.arrayBuffer(),
+      );
     return new ImageResponse( 
         (
             <div style={{position: 'relative', display: 'flex'}}>
                  <img 
                         src={imageData}
                     />
+                 <div tw="flex flex-col w-full h-full absolute">
+                     <div tw="flex h-full w-full">
+                         <div tw="flex flex-col md:flex-row w-full py-12 px-4 justify-between p-8">
+                            <div tw="flex flex-col text-xl sm:text-3xl mt-6 mr-6 ml-6 mb-6 font-bold tracking-tight text-black text-left">
+                                 <span tw="flex flex-col text-lg sm:text-xl md:flex-row w-full py-12 px-4 justify-between p-8" style={{fontFamily: 'Inter-Bold'}}>
+                                    <span tw="bottom-16 right-11" style={{fontFamily: 'Inter-Bold', borderRadius: "50%", overflow: 'hidden'}}>
+                                        <img tw="w-[180px] h-[182px]"
+                                                        src={pfpData}
+                                                /> 
+                                    </span>
+                                 </span>
+                            </div>
+                         </div>
+                     </div>
+                 </div>
             </div>
         )
     )
