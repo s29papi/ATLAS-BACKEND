c2284a535169fd513466b38daf6f9bd6bc12ae71
S29 <nots29vv@gmail.com>
2024-03-03 12:55:54 +0100

    added async to stakepage function

--- frame/match/app/~/stake/page.tsx
+++ frame/match/app/~/stake/page.tsx
@@ -53,7 +53,7 @@ export default function StakePage({ params, searchParams }: Props) {
       let estimateGas = await ethersProvider.estimateGas({
          to: `0x${"47dEAF612F0769d99aDB653bA2d22bba79F26C42"}`,
          value: parseEther("0.2"), 
-         data: fid_string.
+         data: fid_string
         });
       // let sentTx = await signer.sendTransaction({
       //    to: `0x${"47dEAF612F0769d99aDB653bA2d22bba79F26C42"}`, 
