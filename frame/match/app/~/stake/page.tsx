4ee3f66649effd1c735f7463a203cf82aa9abe35
S29 <nots29vv@gmail.com>
2024-03-03 12:26:06 +0100

    added async to stakepage function

--- frame/match/app/~/stake/page.tsx
+++ frame/match/app/~/stake/page.tsx
@@ -45,23 +45,24 @@ export default function StakePage({ params, searchParams }: Props) {
 
 
     async function submitTx() { 
-      if (!walletProvider) throw Error('Wallet Provider Abscent')
-      const ethersProvider = new BrowserProvider(walletProvider)
-      const signer = await ethersProvider.getSigner()
-      let estimateGas = await ethersProvider.estimateGas({
-         to: `0x${"47dEAF612F0769d99aDB653bA2d22bba79F26C42"}`,
-         value: parseEther("0.2"), 
+      console.log(fid)
+      // if (!walletProvider) throw Error('Wallet Provider Abscent')
+      // const ethersProvider = new BrowserProvider(walletProvider)
+      // const signer = await ethersProvider.getSigner()
+      // let estimateGas = await ethersProvider.estimateGas({
+      //    to: `0x${"47dEAF612F0769d99aDB653bA2d22bba79F26C42"}`,
+      //    value: parseEther("0.2"), 
         
-        });
-      let sentTx = await signer.sendTransaction({
-         to: `0x${"47dEAF612F0769d99aDB653bA2d22bba79F26C42"}`, 
-         value: parseEther("0.2"), 
-         gasLimit: estimateGas
-        });
-      let resolvedTx = await sentTx.wait()
+      //   });
+      // let sentTx = await signer.sendTransaction({
+      //    to: `0x${"47dEAF612F0769d99aDB653bA2d22bba79F26C42"}`, 
+      //    value: parseEther("0.2"), 
+      //    gasLimit: estimateGas
+      //   });
+      // let resolvedTx = await sentTx.wait()
       // we keep id in the cookie
       // db stores id and tx hash
-      console.log(resolvedTx?.hash)
+      // console.log(resolvedTx?.hash)
       
      }
     
