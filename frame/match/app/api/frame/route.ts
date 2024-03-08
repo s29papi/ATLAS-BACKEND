3cf21742230607b53c18c581783ad6c9822674dc
S29 <nots29vv@gmail.com>
2024-03-08 15:42:37 +0100

    inframe tx

--- frame/match/app/api/frame/route.ts
+++ frame/match/app/api/frame/route.ts
@@ -17,12 +17,13 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   if (buttonId == 1) {
     return new NextResponse(`<!DOCTYPE html><html><head>
     <title>Start My Match</title>
-    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame" content="vNext" />        
     <meta property="fc:frame:image" content="https://wag3r-bot.vercel.app/og"/>
     <meta property="fc:frame:button:1" content="Back" />
     <meta property="fc:frame:button:1:action" content="post"/>
     <meta property="fc:frame:button:2" content="Stake" />
     <meta property="fc:frame:button:2:action" content="tx"/>
+    <meta property="fc:frame:button:2:target" content="https://wag3r-bot.vercel.app/api/frame/stake"/>
     <meta property="fc:frame:post_url" content="https://wag3r-bot.vercel.app/api/frame/stake"/>
 </head></html>`);
   }
